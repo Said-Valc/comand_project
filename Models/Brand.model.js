ed178e5075e041d7b2b076879a26dd2f942e8544
Shamil <shamil>
2022-04-22 14:02:39 +0300

    done

---
+++ Models/Brand.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const brandSchema = mongoose.Schema({
+    title: String,
+    description: String
+});
+
+const Brand = mongoose.model('Brand', brandSchema);
+
+module.exports = Brand;
